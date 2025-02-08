291c863b4f7b8cf7581cc372eaaaae8a92055c44
chaitra-samant <chaitrasamant27@gmail.com>
2025-02-08 20:55:16 +0530

    Revamped Landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,58 @@
-import './App.css';
-import './fonts.css';
-import Landing from './components/Landing';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { useAuth0 } from "@auth0/auth0-react";
-import { Orgdashboard } from './components/org_dashboard';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
+import "./fonts.css";
+import Landing from "./components/Landing";
+import { Orgdashboard } from "./components/org_dashboard";
+// import Dashboard from "./components/UserDashboard";
 
-// Create a wrapper component to handle authentication logic
-const MainApp = () => {
+const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isLoading } = useAuth0();
 
-  // Show a loading indicator while checking authentication state
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  // Redirect to Orgdashboard if authenticated, otherwise show Landing
-  return isAuthenticated ? <Orgdashboard /> : <Landing />;
+  return isAuthenticated ? children : <Navigate to="/" />;
+};
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Orgdashboard />
+          </ProtectedRoute>
+        }
+      />
+      {/* <Route path="/d" element={<Dashboard />} /> */}
+    </Routes>
+  );
 };
 
 const App = () => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN; // Corrected variable name
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID; // Corrected variable name
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        redirect_uri: window.location.origin // Use the current origin dynamically
+        redirect_uri: window.location.origin,
       }}
     >
-      <MainApp />
+      <Router>
+        <AppRoutes />
+      </Router>
     </Auth0Provider>
   );
 };
